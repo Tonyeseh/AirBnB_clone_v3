bcb7d1165ec65fbb4d8f9b550d284a487cac42b4
tony <tony@github.com>
2023-01-21 23:27:56 +0000

    updated api/v1/views/index.py
    added new route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,32 @@ def status():
     """
     data = {"status": "OK"}
     return jsonify(data)
+
+@app_views.route('/stats')
+def stats():
+    """
+    returns the number of all objects in JSON format
+    """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    
+    classes = {"amenities": Amenity,
+            "cities": City,
+            "places": Place, 
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
+    class_count = {}
+    for key, cls in classes.items():
+        if storage.count(cls) is None:
+            class_count[key] = 0
+        else:
+            class_count[key] = storage.count(cls)
+
+    return jsonify(class_count)
