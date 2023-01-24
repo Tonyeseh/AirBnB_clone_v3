3c1112cfa320801f8365891453f2d9461f11297d
tony <tony@computer.com>
2023-01-25 00:47:00 +0100

    updated amenities module
    fixed styling

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -24,6 +24,7 @@ def amenities():
         amenity.save()
         return jsonify(amenity.to_dict()), 201
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'])
 def get_amenity(amenity_id):
     """gets a specific amenity displays, delete or update it"""
